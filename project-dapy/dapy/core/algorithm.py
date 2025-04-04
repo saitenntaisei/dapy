0d74e3318e81ef7c37ba79478fc88f6b1e9ba744
xdefago <defago@c.titech.ac.jp>
2025-04-04 15:23:45 +0900

    add default methods for State

--- project-dapy/dapy/core/algorithm.py
+++ project-dapy/dapy/core/algorithm.py
@@ -3,6 +3,7 @@ from abc import ABC, abstractmethod
 from .pid import Pid
 from .system import SynchronyModel, System
 from .event import Event
+from typing import Self, Iterable, Optional
 
 
 @dataclass(frozen=True)
@@ -11,6 +12,25 @@ class State(ABC):
     Abstract class to represent the state of an algorithm.
     """
     pid: Pid
+    
+    def copy(self, **kwargs) -> Self:
+        """
+        Create a copy of the state with updated attributes.
+        """
+        return self.__class__(**{**self.__dict__, **kwargs})
+    
+    def as_str(self, keys: Optional[Iterable[str]] = None) -> str:
+        """
+        String representation of the state.
+        """
+        keys = self.__dict__.keys() if keys is None else keys
+        return f"{self.pid}: " + ", ".join(f"{k}={str(self.__dict__.get(k))}" for k in keys if k != "pid")
+    
+    def __str__(self) -> str:
+        """
+        String representation of the state.
+        """
+        return self.as_str()
     
 
 @dataclass(frozen=True)
